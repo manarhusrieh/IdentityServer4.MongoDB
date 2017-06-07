4ee324af204560758cbd5458624d3fa8a1fb6da0
Jing Wu <wjshome@gmail.com>
2017-06-07 15:16:36 +1000

    updated to resolve config DB dynamically in cors policy service to matching IdentityServer4.EntityFramework commit 5402c24e834a385fe5bb067992b1bd2a95eba935 (20/Mar/2017)

--- src/IdentityServer4.MongoDB/Services/CorsPolicyService.cs
+++ src/IdentityServer4.MongoDB/Services/CorsPolicyService.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.MongoDB.Repositories;
 using IdentityServer4.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
@@ -11,18 +13,21 @@ namespace IdentityServer4.MongoDB.Services
 {
     public class CorsPolicyService : ICorsPolicyService
     {
-        private readonly IRepository<Entities.Client> _clientRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ILogger<CorsPolicyService> _logger;
 
-        public CorsPolicyService(IRepository<Entities.Client> clientRepository, ILogger<CorsPolicyService> logger)
+        public CorsPolicyService(IHttpContextAccessor httpContextAccessor, ILogger<CorsPolicyService> logger)
         {
-            _clientRepository = clientRepository;
+            _httpContextAccessor = httpContextAccessor;
             _logger = logger;
         }
 
         public async Task<bool> IsOriginAllowedAsync(string origin)
         {
-            var origins = await _clientRepository.AsQueryable().SelectMany(x => x.AllowedCorsOrigins).Select(x => x.Origin).ToListAsync().ConfigureAwait(false);
+            // doing this here and not in the ctor because: https://github.com/aspnet/CORS/issues/105
+            var clientRepository = _httpContextAccessor.HttpContext.RequestServices.GetRequiredService<IRepository<Entities.Client>>();
+
+            var origins = await clientRepository.AsQueryable().SelectMany(x => x.AllowedCorsOrigins).Select(x => x.Origin).ToListAsync().ConfigureAwait(false);
             var distinctOrigins = origins.Where(x => x != null).Distinct();
             var isAllowed = distinctOrigins.Contains(origin, StringComparer.OrdinalIgnoreCase);
 
