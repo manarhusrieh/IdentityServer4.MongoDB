4d8e0c33fd2bb3d8e8e22fc0cf33a4a1f91837a4
Jing Wu <wjshome@gmail.com>
2018-04-11 17:39:27 +1000

    updated client store tests

--- test/IdentityServer4.MongoDB.Tests/Stores/ClientStoreTests.cs
+++ test/IdentityServer4.MongoDB.Tests/Stores/ClientStoreTests.cs
@@ -37,5 +37,36 @@ namespace IdentityServer4.MongoDB.Tests.Stores
             // Assert
             Assert.NotNull(client);
         }
+
+        [Fact]
+        public async Task FindClientByIdAsync_WhenClientExists_ExpectClientPropertiesRetured()
+        {
+            // Arrange
+            var testClient = new Models.Client
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                ClientName = Guid.NewGuid().ToString(),
+                Properties =
+                {
+                    {"foo1", "bar1"},
+                    {"foo2", "bar2"},
+                }
+            };
+            var mapper = _hostContainer.Container.Resolve<IMapper>();
+            var repository = _hostContainer.Container.Resolve<IRepository<Entities.Client>>();
+            await repository.AddAsync(mapper.Map<Entities.Client>(testClient)).ConfigureAwait(false);
+
+            // Assert
+            var clientStore = _hostContainer.Container.Resolve<IClientStore>();
+            var client = await clientStore.FindClientByIdAsync(testClient.ClientId).ConfigureAwait(false);
+
+            // Assert
+            Assert.NotNull(client);
+            Assert.Equal(2, client.Properties.Count);
+            Assert.True(client.Properties.ContainsKey("foo1"));
+            Assert.True(client.Properties.ContainsKey("foo2"));
+            Assert.Equal("bar1", client.Properties["foo1"]);
+            Assert.Equal("bar2", client.Properties["foo2"]);
+        }
     }
 }
